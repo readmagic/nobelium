a2ec68df32a74f180cf228099f27d4ede67a89bd
frandy <303734023@qq.com>
2024-04-13 20:29:48 +0800

    Initial commit

--- public/geek/index.py
+++ public/geek/index.py
@@ -25,7 +25,7 @@ def generate_index_html():
         file_content += "<h1>Index of Folders</h1>\n"
         file_content += "<ul>\n"
         for file in files:
-            file_content += f"<li><a href='./geek/{folder}/{file}'>{file}</a></li>\n"
+            file_content += f"<li><a href='./{folder}/{file}'>{file}</a></li>\n"
         file_content += "</ul>\n</body>\n</html>"
         with open(folder_path + "/index.html", "w") as f:
             f.write(file_content)
