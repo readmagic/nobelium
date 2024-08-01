36b8cc14f292b40275c3e55f10d66c9c5535ad69
frandy <303734023@qq.com>
2024-08-01 18:46:05 +0800

    Initial commit

--- public/geek/index.py
+++ public/geek/index.py
@@ -16,18 +16,18 @@ def generate_index_html():
     for folder in folders:
         folder_path = os.path.join(current_dir, folder)
         # 创建每个文件夹的链接
-        html_content += f"<li><a href='./geek/{folder}'>{folder}</a></li>\n"
+        html_content += f"<li><a href='./geek/{folder}' target='_blank'>{folder}</a></li>\n"
         folder_path = os.path.join(current_dir, folder)
         # 获取文件夹中的所有文件
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
         # 生成 HTML 内容
         file_content = "<!DOCTYPE html>\n<html>\n<head>\n<title>Index of Folders</title>\n</head>\n<body>\n"
-        file_content += "<h1>Index of Folders</h1>\n"
+        file_content += "<h1>"+folder+"</h1>\n"
         file_content += "<ul>\n"
         for file in sorted(files):
             if file =='index.html':
                 continue
-            file_content += f"<li><a href='./{folder}/{file}'>{file}</a></li>\n"
+            file_content += f"<li><a href='./{folder}/{file} target='_blank''>{file}</a></li>\n"
         file_content += "</ul>\n</body>\n</html>"
         with open(folder_path + "/index.html", "w") as f:
             f.write(file_content)
