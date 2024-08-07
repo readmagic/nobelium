0c95508d549283cd4c61ace872bc515baab8eb01
frandy <303734023@qq.com>
2024-08-07 11:09:41 +0800

    Initial commit

--- public/geek/index.py
+++ public/geek/index.py
@@ -3,8 +3,6 @@ import os
 from urllib import parse
 
 
-
-
 def generate_index_html():
     # 获取当前目录
     current_dir = os.getcwd()
@@ -25,19 +23,26 @@ def generate_index_html():
         # 获取文件夹中的所有文件
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
         # 生成 HTML 内容
-        file_content = "<!DOCTYPE html>\n<html>\n<head>\n<title>"+folder+"</title>\n</head>\n<body>\n"
+        file_content = "<!DOCTYPE html>\n<html>\n<head>\n<title>" + folder + "</title>\n</head>\n<body>\n"
         file_content += "<h1>" + folder + "</h1>\n"
         file_content += "<ul>\n"
         for file in sorted(files):
             if file == 'index.html':
                 continue
-            x = parse.quote(folder)+"/"+parse.quote(file)
+            x = parse.quote(folder) + "/" + parse.quote(file)
             file_content += f"<li><a href='./{x}' target='_blank'>{file}</a></li>\n"
+            with open(folder_path + "/" + file, "r") as x:
+                content = x.read()
+                if not '<meta name="referrer" content="no-referrer" />' in content:
+                    with open(folder_path + "/" + file, "w") as y:
+                        y.write('<meta name="referrer" content="no-referrer" />' + content)
+
         file_content += "</ul>\n"
         file_content += "</body>\n</html>"
 
         with open(folder_path + "/index.html", "w") as f:
             f.write(file_content)
+
     html_content += "</ul>\n</body>\n</html>"
 
     with open("index.html", "w") as f:
